7a46472fefc4cf6189585c90e1ff1ceef9e7daef
zinevych-pja <164073761+zinevych-pja@users.noreply.github.com>
2025-05-05 15:23:28 +0200

    Cw 05.05

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public string mainWord = "default";
+    public TextMeshProUGUI mainWordText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainWordText.text = mainWord;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
